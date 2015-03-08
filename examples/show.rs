e2a758d9473e0fca18939b1c23aa0847c3e30422
Mika Attila <radiantstatue@gmail.com>
2015-03-08 17:14:52 +0100

    Update `show` example

--- examples/show.rs
+++ examples/show.rs
@@ -2,6 +2,8 @@ extern crate libnotify;
 
 fn main() {
     let notify = libnotify::Context::new("hello").unwrap();
-    let n = notify.new_notification("Hello, ", Some("World!"), None).unwrap();
+    let n = notify.new_notification("This is the summary.",
+                                    Some("This is the optional body text."),
+                                    None).unwrap();
     n.show().unwrap();
 }
