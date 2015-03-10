d3ad5323fb775a117d22804c1fb6fad025713670
Mika Attila <radiantstatue@gmail.com>
2015-03-10 19:18:44 +0100

    Update doc example

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,16 @@
 //! extern crate libnotify;
 //!
 //! fn main() {
-//!    let notify = libnotify::Context::new("hello").unwrap();
-//!    let n = notify.new_notification("This is the summary.",
-//!                                    Some("This is the optional body text."),
-//!                                    None).unwrap();
-//!    n.show().unwrap();
+//!     let notify = libnotify::Context::new("hello").unwrap_or_else(|e| {
+//!         panic!("{}", e);
+//!     });
+//!     let body_text = Some("This is the optional body text.");
+//!     let n = notify.new_notification("This is the summary.",
+//!                                     body_text,
+//!                                     None).unwrap_or_else(|e| {
+//!         panic!("{}", e);
+//!     });
+//!     n.show().ok().expect("Failed to show notification");
 //! }
 //! ```
 
